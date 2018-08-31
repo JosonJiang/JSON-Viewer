355702d35e576f90004ed5df40ecb8dd0b4af650
Joson Jiang <87592776@qq.com>
2018-08-31 13:37:09 +0800

    JosonJIang

---
+++ JsonView/JosonView.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPocalipse.Json.JsonView
+{
+    public class JosonView
+    {
+        public JosonView()
+        {
+            string str = @"{ clearOnPageLoad: true,
+        fields: [
+                    {
+                        name:
+                        'Name',
+                        type:
+                        'string'
+                    }
+                ,
+                    {
+                        name:
+                        'Account',
+                        type:
+                        'string'
+                    }
+                ,
+                    {
+                        name:
+                        'HRID',
+                        type:
+                        'string'
+                    }
+                ,
+                    {
+                        name:
+                        'Mobile',
+                        type:
+                        'string'
+                    }
+                ,
+                    {
+                        name:
+                        'HomePhone',
+                        type:
+                        'string'
+                    }
+                    ,
+                        {
+                            name:
+                            'OfficePhone',
+                            type:
+                            'string'
+                        }
+                 ,
+                {
+                    name:
+                    'Mail',
+                    type:
+                    'string'
+                }
+            ]
+    }";
+
+        }
+    }
+}
